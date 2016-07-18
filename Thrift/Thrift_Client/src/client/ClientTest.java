34cda52e66dd3c080de9ab29268d3a016e0bd4cb
xiaofan <zeng-dream@live.com>
2016-07-18 19:52:24 +0800

    TMultiplexedProtocol

--- Thrift/Thrift_Client/src/client/ClientTest.java
+++ Thrift/Thrift_Client/src/client/ClientTest.java
@@ -10,13 +10,14 @@ package client;
 
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TMultiplexedProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.junit.Test;
 
-import thrift.ThriftHelloWorld;
+import thrift.IThriftService;
 
 /**
  * 客户端实现
@@ -33,15 +34,18 @@ public class ClientTest {
         try {
 
             // 设置调用的服务地址为本地，端口为6789
-            TTransport transport = new TSocket("localhost", 6789);
+            TTransport transport = new TSocket("localhost", 1234);
             transport.open();
 
             // 数据传输协议有：二进制协议、压缩协议、JSON格式协议
             // 这里使用的是二进制协议
             // 协议要和服务端一致
             TProtocol protocol = new TBinaryProtocol(transport);
-            ThriftHelloWorld.Client client = new ThriftHelloWorld.Client(protocol);
-
+            // 单接口
+            // IThriftService.Client client = new IThriftService.Client(protocol);
+            // 多接口调用
+            TMultiplexedProtocol mp1 = new TMultiplexedProtocol(protocol, "HelloThrift");
+            IThriftService.Client client = new IThriftService.Client(mp1);
             // 调用服务器端的服务方法
             System.out.println(client.sayHello("zxiaofan"));
 
